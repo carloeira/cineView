2088fdca18dff5437de612e02b80933f4f2080e5
carloeira <augustovieira.cb@gmail.com>
2023-09-24 19:16:45 -0300

    ajuste no search

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  reqUrl: 'https://api.themoviedb.org/3/movie/',
+  reqUrl: 'https://api.themoviedb.org/3/movie',
   searchUrl: 'https://api.themoviedb.org/3/search/movie',
   api: 'https://api.themoviedb.org/3',
   apikey: '?api_key=4b9b981d82a4d05978e4f1d9cb50b01e',
