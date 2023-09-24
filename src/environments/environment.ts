b21e4ef672cd4eb8e0024edb48a8fa8d2f93900d
carloeira <augustovieira.cb@gmail.com>
2023-09-24 12:51:30 -0300

    environment data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  reqUrl: '',
-  searchUrl: '',
-  api: '',
-  apikey: '',
-  language: '',
-  imageUrl: '',
+  reqUrl: 'https://api.themoviedb.org/3/movie/',
+  searchUrl: 'https://api.themoviedb.org/3/search/movie',
+  api: 'https://api.themoviedb.org/3',
+  apikey: '?api_key=4b9b981d82a4d05978e4f1d9cb50b01e',
+  language: '&language=pt-BR',
+  imageUrl: 'https://image.tmdb.org/t/p/original',
 };
