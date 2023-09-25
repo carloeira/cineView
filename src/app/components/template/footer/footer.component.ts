50540c8e20a1fcbe8aa56e3951aafacd7c16b745
carloeira <augustovieira.cb@gmail.com>
2023-09-25 09:46:53 -0300

    adicionando link no footer

--- src/app/components/template/footer/footer.component.ts
+++ src/app/components/template/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  linkedin = environment.linkedin
+  constructor() {}
 
+  ngOnInit(): void {}
 }
